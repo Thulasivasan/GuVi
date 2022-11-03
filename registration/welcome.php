15af307a78f6ec8fc292b6abd4e32a5ffafe28f8
Thulasivasan_v <97501455+Thulasivasan@users.noreply.github.com>
2022-11-03 11:06:12 +0530

    Add files via upload

---
+++ registration/welcome.php
@@ -0,0 +1,17 @@
+
+
+
+<h1>
+	<?php 
+
+
+if (isset($_POST["welcome"])) {
+			$email = $_POST['email'];
+			$password = $_POST['password'];
+			echo "Login Successfull <br>";
+			echo "Welcome Back <br>";
+
+			echo($email);
+
+		} ?>
+</h1>
